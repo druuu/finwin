c59069c0b8aea4a89a9e702c14d889ec1bb723b8
druuu <druuu.mail@gmail.com>
2018-05-07 08:10:50 +0000

    wip: update jupyterlab

--- scripts/update_jupyterlab.sh
+++ scripts/update_jupyterlab.sh
@@ -6,28 +6,49 @@ manifest='efef0e1cf0d326b57c62' &&
 vendor='17b64ee294d1cf4007bb' &&
 main='75795a75c2b565285208' &&
 tmpdir='/tmp/jupyterlab' &&
-appdir='/usr/local/share/jupyter/lab' &&
+tmpdir2='/tmp/lab' &&
 dstdir='/usr/local/lib/python3.5/dist-packages/jupyterlab' &&
+dstdir2='/usr/local/share/jupyter/lab' &&
 uuid="$(uuidgen)" &&
 
 [ -d "$tmpdir" ] && rm -r $tmpdir;
+[ -d "$tmpdir2" ] && rm -r $tmpdir2;
 [ -d "$tmpdir" ] || cp -r ../jupyterlab $tmpdir &&
+[ -d "$tmpdir2" ] || cp -r ../lab $tmpdir2 &&
 
-grep $manifest "$tmpdir"/static/index.html &&
-grep $vendor "$tmpdir"/static/index.html &&
-grep $main "$tmpdir"/static/index.html &&
+grep -iRl "manifest.""$manifest"".js" "$tmpdir"/static | xargs sed --posix -i "s#manifest.""$manifest"".js#manifest.""$uuid"".js#" &&
+grep -iRl "vendor.""$vendor"".js" "$tmpdir"/static | xargs sed --posix -i "s#vendor.""$vendor"".js#vendor.""$uuid"".js#" &&
+grep -iRl "main.""$main"".js" "$tmpdir"/static | xargs sed --posix -i "s#main.""$main"".js#main.""$uuid"".js#" &&
 
-sed --posix -i "s#{{base_url}}lab/static/manifest.""$manifest"".js#https://druuu.me/lab/static""$uuid""/manifest.""$manifest"".js#" "$tmpdir"/static/index.html &&
-sed --posix -i "s#{{base_url}}lab/static/vendor.""$vendor"".js#https://druuu.me/lab/static""$uuid""/vendor.""$vendor"".js#" "$tmpdir"/static/index.html &&
-sed --posix -i "s#{{base_url}}lab/static/main.""$main"".js#https://druuu.me/lab/static""$uuid""/main.""$main"".js#" "$tmpdir"/static/index.html &&
+grep -iRl "manifest.""$manifest"".js" "$tmpdir2"/static | xargs sed --posix -i "s#manifest.""$manifest"".js#manifest.""$uuid"".js#" &&
+grep -iRl "vendor.""$vendor"".js" "$tmpdir2"/static | xargs sed --posix -i "s#vendor.""$vendor"".js#vendor.""$uuid"".js#" &&
+grep -iRl "main.""$main"".js" "$tmpdir2"/static | xargs sed --posix -i "s#main.""$main"".js#main.""$uuid"".js#" &&
 
-sed --posix -i "s#{{base_url}}lab/static/manifest.""$manifest"".js#https://druuu.me/lab/static""$uuid""/manifest.""$manifest"".js#" "$appdir"/static/index.html &&
-sed --posix -i "s#{{base_url}}lab/static/vendor.""$vendor"".js#https://druuu.me/lab/static""$uuid""/vendor.""$vendor"".js#" "$appdir"/static/index.html &&
-sed --posix -i "s#{{base_url}}lab/static/main.""$main"".js#https://druuu.me/lab/static""$uuid""/main.""$main"".js#" "$appdir"/static/index.html &&
+sed --posix -i "s#{{base_url}}lab/static/manifest.""$uuid"".js#https://druuu.me/lab/static/manifest.""$uuid"".js#" "$tmpdir"/static/index.html &&
+sed --posix -i "s#{{base_url}}lab/static/vendor.""$uuid"".js#https://druuu.me/lab/static/vendor.""$uuid"".js#" "$tmpdir"/static/index.html &&
+sed --posix -i "s#{{base_url}}lab/static/main.""$uuid"".js#https://druuu.me/lab/static/main.""$uuid"".js#" "$tmpdir"/static/index.html &&
 
-mv "$tmpdir"/static2 "$tmpdir"/static"$uuid" &&
+sed --posix -i "s#{{base_url}}lab/static/manifest.""$uuid"".js#https://druuu.me/lab/static/manifest.""$uuid"".js#" "$tmpdir2"/static/index.html &&
+sed --posix -i "s#{{base_url}}lab/static/vendor.""$uuid"".js#https://druuu.me/lab/static/vendor.""$uuid"".js#" "$tmpdir2"/static/index.html &&
+sed --posix -i "s#{{base_url}}lab/static/main.""$uuid"".js#https://druuu.me/lab/static/main.""$uuid"".js#" "$tmpdir2"/static/index.html &&
+
+mv "$tmpdir"/static/manifest."$manifest".js "$tmpdir"/static/manifest."$uuid".js &&
+mv "$tmpdir"/static/manifest."$manifest".js.map "$tmpdir"/static/manifest."$uuid".js.map &&
+mv "$tmpdir"/static/vendor."$vendor".js "$tmpdir"/static/vendor."$uuid".js &&
+mv "$tmpdir"/static/vendor."$vendor".js.map "$tmpdir"/static/vendor."$uuid".js.map &&
+mv "$tmpdir"/static/main."$main".js "$tmpdir"/static/main."$uuid".js &&
+mv "$tmpdir"/static/main."$main".js.map "$tmpdir"/static/main."$uuid".js.map &&
+
+mv "$tmpdir2"/static/manifest."$manifest".js "$tmpdir2"/static/manifest."$uuid".js &&
+mv "$tmpdir2"/static/manifest."$manifest".js.map "$tmpdir2"/static/manifest."$uuid".js.map &&
+mv "$tmpdir2"/static/vendor."$vendor".js "$tmpdir2"/static/vendor."$uuid".js &&
+mv "$tmpdir2"/static/vendor."$vendor".js.map "$tmpdir2"/static/vendor."$uuid".js.map &&
+mv "$tmpdir2"/static/main."$main".js "$tmpdir2"/static/main."$uuid".js &&
+mv "$tmpdir2"/static/main."$main".js.map "$tmpdir2"/static/main."$uuid".js.map &&
+
 
 rm -r "$dstdir" &&
-rm -r "$appdir" &&
 cp -r "$tmpdir" "$dstdir" &&
-cp -r "$tmpdir"/static"$uuid" "$appdir"/
+
+rm -r "$dstdir2"/static &&
+cp -r "$tmpdir2"/static "$dstdir2"/static
